ae8ba69a79e9dafb8ffeb84adf1f9c5fa4b8324c
tomas_agence <tomas.sanhueza@sp.agence.com.br>
2024-10-11 23:51:10 -0300

    feat: finish MVP

--- src/main/java/com/timix/todo/back/modules/todo/controller/UpdateToDoController.java
+++ src/main/java/com/timix/todo/back/modules/todo/controller/UpdateToDoController.java
@@ -1,18 +1,14 @@
 package com.timix.todo.back.modules.todo.controller;
-
-import com.timix.todo.back.modules.todo.Dto.CreateToDoDTO;
-import com.timix.todo.back.modules.todo.services.CreateToDoService;
 import com.timix.todo.back.modules.todo.services.UpdateToDoService;
 import com.timix.todo.back.utils.Utils;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
-/
+
 @RestController
 @RequestMapping("/todo")
 public class UpdateToDoController {
